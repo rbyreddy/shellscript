3c67315e99ae734e756e0415d4d51f8ea37d12f4
Rakesh <byreddyrakesh41@gmail.com>
2022-09-17 11:28:10 +0530

    [rocky] updated

---
+++ case/case.sh
@@ -0,0 +1,22 @@
+dayvalue=5
+case $dayvalue in
+	1) echo "monday"
+	;;
+	2) echo "tuesday"
+        ;;
+	3) echo "wednesday"
+	;;	
+	4) echo "thursday"
+	;;
+	5) echo "friday"
+	;;
+	6) echo "saturday"
+	;;
+	7) echo "sunday"
+	;;
+	*) echo "enter the correct value"
+	;;
+esac
+
+
+
