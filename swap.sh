acebf04f767f2d751470ad104a613218bb5afb9c
Rakesh <byreddyrakesh41@gmail.com>
2022-09-21 08:42:23 +0530

    review 2

---
+++ swap.sh
@@ -0,0 +1,12 @@
+#! /bin/bash -x
+# Static input of the
+# number
+first=5
+second=10
+   
+temp=$first
+first=$second
+second=$temp
+  
+echo "After swapping, numbers are:"
+echo "first = $first, second = $second"
